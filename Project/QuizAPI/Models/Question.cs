2c05d38725a51473c6ea320009132f0783a7b668
Gizem SARITAŞ <ymgcon@hotmail.com>
2022-03-14 13:03:27 +0300

    create basic api context

---
+++ Project/QuizAPI/Models/Question.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QuizAPI.Models
+{
+    public class Question
+    {
+        [Key]
+        public int QnId { get; set; }
+        [Column(TypeName = "nvarchar(250)")]
+        public string? QnInWords { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public string? ImageName { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public int Options1 { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public int Options2 { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public int Options3 { get; set; }
+        [Column(TypeName = "nvarchar(50)")]
+        public int Options4 { get; set; }
+        public int Answer { get; set; }
+    }
+}
